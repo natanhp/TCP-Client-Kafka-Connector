fce9720ce3538e12c3283ab25627cbbff40c4333
natanhp <natanhp@protonmail.com>
2022-04-02 11:21:33 +0700

    Create simple unit test

--- src/test/java/id/natanhp/kafka/TCPSocketTaskTest.java
+++ src/test/java/id/natanhp/kafka/TCPSocketTaskTest.java
@@ -1,39 +1,67 @@
 package id.natanhp.kafka;
 
-import org.apache.kafka.connect.source.SourceRecord;
-import org.junit.jupiter.api.AfterAll;
-import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.util.List;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Map;
 
-public class TCPSocketTaskTest {
-    static TCPSocketTask tcpSocketTask;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
-    @BeforeAll
-    static void setup() {
+public class TCPSocketTaskTest {
+    private ServerSocket serverSocket;
+    private Socket clientSocket;
+    private PrintWriter out;
+    private int serverPort;
+    private TCPSocketTask tcpSocketTask;
+
+    @BeforeEach
+    void init() {
+        try {
+            serverSocket = new ServerSocket(0);
+            serverPort = serverSocket.getLocalPort();
+            new Thread(() -> {
+                try {
+                    clientSocket = serverSocket.accept();
+                    out = new PrintWriter(clientSocket.getOutputStream(), true);
+                    out.println("A");
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
         tcpSocketTask = new TCPSocketTask();
+
         tcpSocketTask.start(Map.ofEntries(
                 Map.entry(TCPSocketConnector.HOST, "localhost"),
-                Map.entry(TCPSocketConnector.PORT, "10000"),
-                Map.entry(TCPSocketConnector.TOPIC, "socket_topic")
+                Map.entry(TCPSocketConnector.PORT, String.valueOf(serverPort)),
+                Map.entry(TCPSocketConnector.TOPIC, "topic")
         ));
     }
 
-//    @Test
-    void testPoll() {
+    @Test
+    void testReceivingStream() {
         try {
-            List<SourceRecord> test = tcpSocketTask.poll();
-
-            test.forEach(t -> System.out.println(t.toString()));
+            assertEquals(tcpSocketTask.poll().get(0).value(), "A");
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
     }
 
-    @AfterAll
-    static void stop() {
+    @AfterEach
+    void tearDown() {
         tcpSocketTask.stop();
+        try {
+            serverSocket.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
